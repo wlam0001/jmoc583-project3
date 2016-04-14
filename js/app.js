c5dba1bee9be5371210a84ad11f938e58092bbd6
Emily Lam <wlam0001@live.unc.edu>
2016-04-14 15:02:24 -0400

    Got picture display and pie overlap to overlap

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function startup() {
         .sort(null);
 
     var arc = d3.svg.arc()
-        .innerRadius(radius - 60)
+        .innerRadius(radius - 100)
         .outerRadius(radius - 10);
 
     var svg = d3.select("body").append("svg")
@@ -88,34 +88,71 @@ function startup() {
         pie.value(function(d) {
             return 1;
         });
-        var specificChampions = [];
-        champion.forEach(function(champ) {
-          var champTag = champ.tags;
-          if(contains(champTag, leagueTag)){
-            specificChampions.push(champ);
-          }
-        });
+        var specificChampions = specificChamps(champion, leagueTag);
 
         g = g.data(pie(specificChampions), function(d) {
             return d.data.name;
         });
-        g.enter().append("path").attr("fill", function(d) {
-            return color(d.data.name);
-        });
+
+        g.enter().append("path")
+            .attr("fill", function(d) {
+                return color(d.data.name);
+            })
+            .on('mouseover', function(d) {
+                if (leagueTag == "Fighter") {
+                    d3.select(".tooltip")
+                        .style("left", d3.event.pageX + "px")
+                        .style("top", d3.event.pageY + "px")
+                        .style("opacity", 1)
+                        .html(d.data.name);
+                }
+                d3.select(this)
+                    .style('opacity', 0.3);
+            })
+            .on('mouseout', function(d) {
+                d3.select(this)
+                    .style('opacity', 1);
+            })
+            .on("click", function(d) {
+                d3.select(".chart1")
+                    .html(d.data.name + '<img src="' + d.data.image +  '" />');
+
+            });
+
         g.exit().remove();
         g.attr("d", arc);
 
+        if (leagueTag != "Fighter") {
+            g.enter().append("text")
+                .attr("transform", function(d) {
+                    return "translate(" + arc.centroid(d) + ")";
+                })
+                .attr("text-anchor", "middle")
+                .text(function(d) {
+                    return d.data.name;
+                });
+        }
+
     }
 }
+
 function contains(tags, certainTag) {
     var i = tags.length;
     while (i--) {
-       if (tags[0] == certainTag) {
-           return true;
-       }
+        if (tags[0] == certainTag) {
+            return true;
+        }
     }
     return false;
 }
-fucntion specificChampions(champions){
 
+function specificChamps(champions, tags) {
+    var specificChampion = [];
+    champion.forEach(function(champ) {
+        var champTag = champ.tags;
+        if (champTag[0] == tags) {
+            specificChampion.push(champ);
+        }
+    });
+    return specificChampion;
 }
